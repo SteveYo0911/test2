0282c05fc05149072f2cfa76dbdcb47a33692c43
Steve Yo <s29668518@hotmail.com>
2018-01-23 09:21:00 +0800

    master4

--- app/src/main/java/com/example/steve_yo/gittest1/MainActivity.java
+++ app/src/main/java/com/example/steve_yo/gittest1/MainActivity.java
@@ -11,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: master");
+        Log.d(TAG, "onCreate: issure1");
+        Log.d(TAG, "onCreate: issure2");
+
     }
 }
