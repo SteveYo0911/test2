b9ea53706a6e982b76da4e8ebe244b324f14509d
Steve Yo <s29668518@hotmail.com>
2018-01-23 09:14:26 +0800

    issure1

--- app/src/main/java/com/example/steve_yo/gittest1/MainActivity.java
+++ app/src/main/java/com/example/steve_yo/gittest1/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: master");
+        Log.d(TAG, "onCreate: branch1");
         
     }
 }
